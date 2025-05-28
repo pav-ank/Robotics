203227183322e7bf1e9672e22f29b88d69c37932
Pavankumar Megeri <pavankumar26718@gmail.com>
2025-05-28 17:43:36 +0530

    Remove Arduino Libraries from repo and add to .gitignore

---
+++ Arduino/4) RGB LED/rgb_blink/rgb_blink.ino
@@ -0,0 +1,28 @@
+//const int red_pin = 11;
+
+#define RED_PIN   11
+#define BLUE_PIN  5
+#define GREEN_PIN 3
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(GREEN_PIN, LOW);
+  digitalWrite(RED_PIN, HIGH);  
+  delay(1000);                     
+  digitalWrite(RED_PIN, LOW);                       
+  digitalWrite(BLUE_PIN, HIGH);  
+  delay(1000);                     
+  digitalWrite(BLUE_PIN, LOW);   
+  digitalWrite(GREEN_PIN, HIGH);  
+  delay(1000);                   
+     
+  delay(1000);
+}
